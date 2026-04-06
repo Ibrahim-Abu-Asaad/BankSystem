2d2329ce15d4c0cdf0e252c510392619645e7612
Ibrahim-Abu-Assad <i6802275@gmail.com>
2026-04-06 22:42:52 +0300

    Add Fields and Constructors to clsClient

--- BankSystemBLL/clsClient.cs
+++ BankSystemBLL/clsClient.cs
@@ -8,20 +8,52 @@ using System.Threading.Tasks;
 
 namespace BankSystemBLL
 {
-    public class clsClient
+    public class clsClient : clsPerson
     {
 
 
         // Fields
 
-
-
-
+        int ID { get; set; }
+        string AccountNO { get; set; }
+        string PINcode { get; set; }
+        decimal Balance { get; set; }
+        DateTime CreatedAt { get; set; }
 
 
 
         // Constructors
+        public clsClient() : base()
+        {
 
+            this.ID = -1;
+            this.AccountNO = "";
+            this.PINcode = "";
+            this.Balance = 0m;
+            this.CreatedAt = DateTime.Now;
+
+            this.PersonID = -1;
+            this.Name = "";
+            this.Email = "";
+            this.BirthDate = null;
+            this.Address = "";
+            this.ImagePath = "";
+            this.CountryID = -1;
+            this.Phone = "";
+            this.Gender = "Male";
+
+        }
+
+        private clsClient(int PersonID, string Name, string Email, DateTime BirthDate, string Address, string ImagePath, int CountryID, string Phone, int MarkDeleted, string Gender, int ID, string AccountNO, string PINcode, decimal Balance, DateTime CreatedAt) : base(PersonID, Name, Email, BirthDate, Address, ImagePath, CountryID, Phone, MarkDeleted, Gender)
+        {
+
+            this.ID = ID;
+            this.AccountNO = AccountNO;
+            this.PINcode = PINcode;
+            this.Balance = Balance;
+            this.CreatedAt = CreatedAt;
+
+        }
 
 
 
